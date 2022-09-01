f0fcbcee11e5237e7f98f96de4489a8aef23aecc
Kevin Nathaniel Tiscareno Chavez <mygame1980@hotmail.com>
2022-09-01 13:18:52 -0600

    Update 9/1/2022
    Project Started

---
+++ Realm Rush/Assets/Scripts/CoordinateLabeler.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[ExecuteAlways]
+public class CoordinateLabeler : MonoBehaviour
+{
+    TextMeshPro label;
+    Vector2Int coordinates = new Vector2Int(); //Representation of a 2d vector using int
+
+    void Awake() 
+    {
+        label = GetComponent<TextMeshPro>();
+        DisplayCoordinates();    
+    }
+    
+    void Update()
+    {
+        if (!Application.isPlaying)
+        {
+            DisplayCoordinates();
+            UpdateObjectName();
+        }
+    }
+
+    void DisplayCoordinates()
+    {
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
+        label.text = coordinates.x + ", " + coordinates.y;
+    }
+
+    void UpdateObjectName()
+    {
+        transform.parent.name = coordinates.ToString();
+    }
+}
