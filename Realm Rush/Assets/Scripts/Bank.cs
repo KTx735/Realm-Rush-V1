2a3996c8ec071c6ef2c85382ca818d1379683610
Kevin Nathaniel Tiscareno Chavez <mygame1980@hotmail.com>
2022-10-26 08:44:14 -0600

    Update 10/26/2022
    Added:
    - GUI to display "Gold"
    
    Need to Fix:
    - Enemy health does not work correctly (Enemies are still one hit shot and "life" is not incrementing)

--- Realm Rush/Assets/Scripts/Bank.cs
+++ Realm Rush/Assets/Scripts/Bank.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
@@ -10,21 +11,26 @@ public class Bank : MonoBehaviour
     [SerializeField] int currentBalance;
     public int CurrentBalance{get {return currentBalance;} }
 
+    [SerializeField] TextMeshProUGUI displayBalance;
+
     void Awake()
     {
         currentBalance = startingBalance;
+        updateDisplay();
     }
 
     public void Deposit(int amount)
     {
         //Make every amount into positive
         currentBalance += Mathf.Abs(amount);
+        updateDisplay();
     }
 
     public void Withdraw(int amount)
     {
         //Guard from adding more negative
         currentBalance -= Mathf.Abs(amount);
+        updateDisplay();
 
         if (currentBalance < 0)
         {
@@ -38,6 +44,11 @@ public class Bank : MonoBehaviour
     {
         Scene currentScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(currentScene.buildIndex);
+    }
+
+    void updateDisplay()
+    {
+        displayBalance.text = "Gold: " + currentBalance;
     }
 
 }
--- Realm Rush/Assets/Scripts/EnemyHealth.cs
+++ Realm Rush/Assets/Scripts/EnemyHealth.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Enemy))]
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] int maxHitPoints = 20;
+    [Tooltip("Adds amount to maxPoints when enemy dies")]
+    [SerializeField] int difficultyRamp = 1;
     int currentHitPoint = 0;
 
     Enemy enemy;
@@ -29,6 +32,7 @@ public class EnemyHealth : MonoBehaviour
         if(currentHitPoint <= 0)
         {
             gameObject.SetActive(false);
+            maxHitPoints += difficultyRamp;
             enemy.RewardGold();
         }
     }
